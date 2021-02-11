1678666a779d9dd09aa5c408c557afcfee8d8650
lamin <lamin@live.com>
2021-02-11 23:19:02 +0700

    first commit

---
+++ precache-manifest.8801642f770d0aef44c612191c77c5bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7634d2a2f7b98d500e53",
+    "url": "/css/app.32019a13.css"
+  },
+  {
+    "revision": "423917533f94334bc9e50c46cd967d30",
+    "url": "/img/appstore.42391753.png"
+  },
+  {
+    "revision": "eae79764fa2b52788697070cce95faed",
+    "url": "/img/background.eae79764.png"
+  },
+  {
+    "revision": "a1057dcb644d8729f4f76b2032f21743",
+    "url": "/img/check-circle.a1057dcb.gif"
+  },
+  {
+    "revision": "61229179fce69fffc4be31f5b269497b",
+    "url": "/img/novideo.61229179.jpg"
+  },
+  {
+    "revision": "7186ec9f23d7073ba2578efd35111a9f",
+    "url": "/img/previewer.7186ec9f.png"
+  },
+  {
+    "revision": "a0e8c5c577e100e3770cd2a80a1029ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7634d2a2f7b98d500e53",
+    "url": "/js/app.771c7166.js"
+  },
+  {
+    "revision": "ef5365d070c08371d2ce",
+    "url": "/js/chunk-vendors.f8f3fc66.js"
+  },
+  {
+    "revision": "93f38f9f5e0130a8ae05c9ed6abda9b4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
